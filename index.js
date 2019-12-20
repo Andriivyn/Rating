7ca74e3b6af456ece5e3cf6c517feab03ad425ba
anvin23 <andrey1vinnik@gmail.com>
2019-12-20 16:55:19 +0200

    thanks

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ if(isNaN(subj))
 else{
     $(".wrap").addClass("hide");
     $(".grades-page").addClass("show");
-    for (let i=0; i<=subj-1; i++){
-        $(".grades-page").append($(".grades-input"));
-    }
+    let inq = $(".grades-input");
+    console.log(inq);
+        for (let i = 0; i <= subj - 1; i++) {
+            const gradesInput = $("<div>", { "class": "grades-input" });
+            gradesInput.append($("<input>", { type: "text", "class": "grade-in" }));
+            gradesInput.append($("<input>", { type: "text", "class": "credit-in" }));
+            $(".grades-page").append(gradesInput);
+        } 
+    $(".grades-page").append($("<button>", { "class": "count-average", type: "button" }).text("CONFIRM"));
 
 }
 })
-
-
-
+$(".count-avнerage").click(function(){
+})
 })
